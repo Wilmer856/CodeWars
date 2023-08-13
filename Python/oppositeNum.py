70c15a85fa149313763a2c1d608c768afe3abeb6
Wilmer Guzman <joelguzman2468@gmail.com>
2023-08-13 09:21:02 -0400

    Adding coding challenge for 08-13-2023.

---
+++ Python/oppositeNum.py
@@ -0,0 +1,15 @@
+
+
+#DESCRIPTION:
+#Very simple, given a number (integer / decimal / both depending on the language),
+#find its opposite (additive inverse).
+
+# Examples:
+# opposite(1): -1
+# opposite(14): -14
+# opposite(-34): 34
+
+# returns its opposite (additive inverse) by multiplying it by -1.
+# This is because the opposite of a number on the number line is obtained by negating it.
+def opposite(number):
+    return number*-1
