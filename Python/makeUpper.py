a3a00f63f2ff1c9ead036ba72adf167c9e72b628
Wilmer Guzman <joelguzman2468@gmail.com>
2023-08-19 15:12:46 -0400

    Adding coding challenge for 08-19-2023.

---
+++ Python/makeUpper.py
@@ -0,0 +1,9 @@
+
+# DESCRIPTION:
+# Write a function which converts the input string to uppercase.
+
+
+# function 'make_upper_case' that takes a string (s) as input and
+# returns the uppercase version of the string using the upper() method.
+def make_upper_case(s):
+    return s.upper()
