26728e97bcc228ec476cbf7d6662f9966c31a6c5
Wilmer Guzman <joelguzman2468@gmail.com>
2023-08-30 10:31:26 -0400

    Adding coding challenge for 08-30-2023.

---
+++ Python/bool-to-str.py
@@ -0,0 +1,9 @@
+
+
+# DESCRIPTION:
+# Implement a function which convert the given boolean value into its string representation.
+
+def boolean_to_string(b):
+    return str(b)
+
+# The str function converts the boolean value to its string representation.
