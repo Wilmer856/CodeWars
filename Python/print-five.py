b2c0989b0f6a03a4b33d56daf20c5d440141ada8
Wilmer Guzman <joelguzman2468@gmail.com>
2023-09-03 14:53:06 -0400

    Adding new coding challenge for 09-03-2023.

---
+++ Python/print-five.py
@@ -0,0 +1,10 @@
+
+# DESCRIPTION:
+# Write a function that always returns 5
+
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+def unusual_five():
+    return len("Smile")
+
+# returns '5' by taking the length of the string "Smile," which is always 5 characters long.
