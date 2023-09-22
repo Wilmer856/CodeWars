bab99f4c2baef4f975f8d7f367d4f65db77baefc
Wilmer856 <joelguzman2468@gmail.com>
2023-09-22 08:45:21 -0400

    Adding JavaScript folder and coding challenge for 09-22-2023.

---
+++ JavaScript/noSpace.js
@@ -0,0 +1,15 @@
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+    let newX = ""
+    for(let i = 0; i < x.length; i++){
+      if(x[i] === " "){
+        continue
+      }
+      else{
+        newX += x[i]
+      }
+    }
+    return newX
+}
