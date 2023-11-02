4d5d3d024c16b3098d428301bad61a8e4070fb7e
Wilmer856 <joelguzman2468@gmail.com>
2023-11-02 08:11:23 -0400

    Adding coding challenge for 11-02-2023.

---
+++ JavaScript/square.js
@@ -0,0 +1,7 @@
+
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+square = n => n*n;
+
+// A square is simply a number multiplied by itself. 
