89e763b1dd021664a360024fb6d332261379c31a
Wilmer856 <joelguzman2468@gmail.com>
2024-01-15 08:12:45 -0500

    Adding coding challenge for 01-15-2024.

---
+++ JavaScript/6th Kyu/countChar.js
@@ -0,0 +1,26 @@
+
+// DESCRIPTION:
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+    let count = {}
+       
+    string.split('').forEach(char => {
+      if(Object.hasOwn(count, char)) {
+        if(char !== ''){
+          count[char] += 1;
+        } 
+      }
+      else {
+        if(char !== '') {
+          count[char] = 1;
+        } else {
+          count[char] = {};
+        }
+      }
+    })
+    
+    return count;
+}
